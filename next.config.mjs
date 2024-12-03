a0b2212e6e9b97e3af0221b2cff8bd2aaa83a4cf
nattuthurai <nattuthurai.k@live.com>
2024-12-03 18:12:36 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+        API_SECRET:'fe1f54bfe790f712bd4e352c79c4dea09f543932fd8f31279c3b84b5a221125f'
+    }
+};
+
+export default nextConfig;
